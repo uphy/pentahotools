dffa45e28f2378d20b4ca0df0f0c2726aecfa467
Yuhi Ishikura <yuhi.ishikura@gmail.com>
2017-06-08 21:50:40 +0900

    Add command to delete and put files.

--- client/file.go
+++ client/file.go
@@ -1,10 +1,12 @@
 package pentahoclient
 
 import (
-	"errors"
 	"fmt"
+	"io/ioutil"
 	"strconv"
 	"strings"
+
+	"github.com/pkg/errors"
 )
 
 func (c *Client) Backup(output string) error {
@@ -57,6 +59,7 @@ func (c *Client) Tree(path string, depth int, showHidden bool) (*FileEntry, erro
 		SetHeader("Accept", "application/json").
 		SetResult(&root).
 		Get(fmt.Sprintf("api/repo/files/%s/tree", strings.Replace(path, "/", ":", -1)))
+
 	switch resp.StatusCode() {
 	case 200:
 		return &root, nil
@@ -69,6 +72,119 @@ func (c *Client) Tree(path string, depth int, showHidden bool) (*FileEntry, erro
 			return nil, err
 		}
 		return nil, fmt.Errorf("Unknown error. statusCode=%d", resp.StatusCode())
+	}
+}
+
+// GetACL gets the access control list of file.
+func (c *Client) GetACL(path string) (*Acl, error) {
+	var acl Acl
+	resp, err := c.client.R().
+		SetHeader("Accept", "application/json").
+		SetResult(&acl).
+		Get(fmt.Sprintf("api/repo/files/%s/acl", strings.Replace(path, "/", ":", -1)))
+
+	switch resp.StatusCode() {
+	case 200:
+		return &acl, nil
+	case 403:
+		return nil, errors.New("Failed to save acls due to missing or incorrect properties")
+	case 400:
+		return nil, errors.New("Failed to save acls due to malformed xml")
+	case 500:
+		return nil, errors.New("Failed to save acls due to another error")
+	default:
+		if err != nil {
+			return nil, err
+		}
+		return nil, fmt.Errorf("Unknown error. statusCode=%d", resp.StatusCode())
+	}
+}
+
+// PutFile put the file to the repository.
+func (c *Client) PutFile(file string, destination string) error {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return err
+	}
+	resp, err := c.client.R().
+		SetBody(data).
+		Put(fmt.Sprintf("api/repo/files/%s", strings.Replace(destination, "/", ":", -1)))
+
+	switch resp.StatusCode() {
+	case 200:
+		return nil
+	case 403:
+		return errors.New("Failure to create the file due to permissions, file already exists, or invalid path id")
+	case 500:
+		return errors.New("server error")
+	default:
+		if err != nil {
+			return err
+		}
+		return fmt.Errorf("Unknown error. statusCode=%d", resp.StatusCode())
+	}
+}
+
+// GetFile gets file from the repository
+func (c *Client) GetFile(repositoryPath string, destination string) error {
+	resp, err := c.client.R().
+		SetOutput(destination).
+		Get(fmt.Sprintf("api/repo/files/%s", strings.Replace(repositoryPath, "/", ":", -1)))
+
+	switch resp.StatusCode() {
+	case 200:
+		return nil
+	case 403:
+		return errors.New("Failure to create the file due to permissions, file already exists, or invalid path id")
+	case 500:
+		return errors.New("server error")
+	default:
+		if err != nil {
+			return err
+		}
+		return fmt.Errorf("Unknown error. statusCode=%d", resp.StatusCode())
+	}
+}
+
+// DeleteFile delets file from the repository
+func (c *Client) DeleteFile(repositoryPath ...string) error {
+	return c.deleteFile(false, repositoryPath...)
+}
+
+// DeleteFilePermanent delets file from the repository
+func (c *Client) DeleteFilePermanent(repositoryPath ...string) error {
+	return c.deleteFile(true, repositoryPath...)
+}
+
+func (c *Client) deleteFile(permanent bool, repositoryPath ...string) error {
+	ids := make([]string, len(repositoryPath))
+	for i, path := range repositoryPath {
+		acl, err := c.GetACL(path)
+		if err != nil {
+			return errors.Wrap(err, "failed to get the ID of the deleting file:"+path)
+		}
+		ids[i] = acl.ID
+	}
+	var apiPath string
+	if permanent {
+		apiPath = "api/repo/files/deletepermanent"
+	} else {
+		apiPath = "api/repo/files/delete"
+	}
+	resp, err := c.client.R().
+		SetBody(strings.Join(ids, ",")).
+		Put(apiPath)
+
+	switch resp.StatusCode() {
+	case 200:
+		return nil
+	case 500:
+		return errors.New("Failure to move the files specified in the comma-separated list to the trash")
+	default:
+		if err != nil {
+			return err
+		}
+		return fmt.Errorf("Unknown error. statusCode=%d", resp.StatusCode())
 	}
 }
 
@@ -109,4 +225,19 @@ func (e *FileEntry) print(level int) {
 	for _, entry := range e.Children {
 		entry.print(level + 1)
 	}
+}
+
+type Ac struct {
+	Modifiable    string
+	Permissions   string
+	Recipient     string
+	RecipientType string
+}
+
+type Acl struct {
+	Aces              []Ac
+	EntriesInheriting string
+	ID                string
+	Owner             string
+	OwnerType         string
 }
--- cmd/file.go
+++ cmd/file.go
@@ -28,8 +28,11 @@ var fileLsCmd = &cobra.Command{
 		showHidden, _ := cmd.Flags().GetBool("showHidden")
 		depth, _ := cmd.Flags().GetInt("depth")
 		root, err := Client.Tree(path, depth, showHidden)
+		if err != nil {
+			return err
+		}
 		root.Print()
-		return err
+		return nil
 	},
 }
 
@@ -64,12 +67,62 @@ var fileRestoreCmd = &cobra.Command{
 	},
 }
 
+var fileGetCmd = &cobra.Command{
+	Use:   "get",
+	Short: "Get a file from the repository.",
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 2 {
+			return errors.New("specify repository path and destination path")
+		}
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		err := Client.GetFile(args[0], args[1])
+		return err
+	},
+}
+
+var filePutCmd = &cobra.Command{
+	Use:   "put",
+	Short: "Put a file to the repository.",
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 2 {
+			return errors.New("specify input file and destination path")
+		}
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		err := Client.PutFile(args[0], args[1])
+		return err
+	},
+}
+var fileDeleteFileCmd = &cobra.Command{
+	Use:   "delete",
+	Short: "Delete files from to the repository.",
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) == 0 {
+			return errors.New("specify the paths to delete")
+		}
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		err := Client.DeleteFile(args...)
+		return err
+	},
+}
+
 func init() {
 	RootCmd.AddCommand(fileCmd)
+
 	fileLsCmd.Flags().BoolP("showHidden", "s", false, "Show hidden files")
 	fileLsCmd.Flags().IntP("depth", "d", 1, "The depth of the tree")
 	fileCmd.AddCommand(fileLsCmd)
+
 	fileCmd.AddCommand(fileBackupCmd)
+	fileCmd.AddCommand(filePutCmd)
+	fileCmd.AddCommand(fileGetCmd)
+	fileDeleteFileCmd.Flags().BoolP("permanent", "P", false, "Delete file permanently.")
+	fileCmd.AddCommand(fileDeleteFileCmd)
 
 	fileRestoreCmd.Flags().BoolVarP(&overwrite, "overwrite", "o", false, "If kept at the default of true, overwrites any value found on the system with the matching value that is being imported. ")
 	fileCmd.AddCommand(fileRestoreCmd)
