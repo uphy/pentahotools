fdc23262a2f4cc837f781a332018b8fd1062b2a4
Yuhi Ishikura <yuhi.ishikura@gmail.com>
2017-06-19 07:31:50 +0900

    Add console logger.

--- client/client.go
+++ client/client.go
@@ -4,18 +4,52 @@ import (
 	"fmt"
 
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 
 	resty "gopkg.in/resty.v0"
 )
 
 // Logger is a logger for this package
-var Logger *zap.Logger
+var Logger *CompositeLogger
 
 func init() {
-	config := zap.NewDevelopmentConfig()
-	config.OutputPaths = []string{"./pentahotools.log"}
-	config.Level.SetLevel(zap.DebugLevel)
-	Logger, _ = config.Build()
+	fileConfig := zap.NewDevelopmentConfig()
+	fileConfig.OutputPaths = []string{"./pentahotools.log"}
+	fileConfig.Level.SetLevel(zap.DebugLevel)
+	fileLogger, _ := fileConfig.Build()
+
+	consoleConfig := zap.NewDevelopmentConfig()
+	consoleConfig.Encoding = "console"
+	consoleConfig.EncoderConfig.TimeKey = ""
+	consoleConfig.EncoderConfig.StacktraceKey = ""
+	consoleConfig.OutputPaths = []string{"stdout"}
+	consoleConfig.Level.SetLevel(zap.WarnLevel)
+	consoleLogger, _ := consoleConfig.Build()
+
+	Logger = newCompositeLogger(*consoleLogger, *fileLogger)
+}
+
+// CompositeLogger merges multiple loggers.
+type CompositeLogger struct {
+	Logger []zap.Logger
+}
+
+func newCompositeLogger(logger ...zap.Logger) *CompositeLogger {
+	return &CompositeLogger{logger}
+}
+
+// Debug records the debug logs
+func (l *CompositeLogger) Debug(msg string, fields ...zapcore.Field) {
+	for _, logger := range l.Logger {
+		logger.Debug(msg, fields...)
+	}
+}
+
+// Warn records the debug logs
+func (l *CompositeLogger) Warn(msg string, fields ...zapcore.Field) {
+	for _, logger := range l.Logger {
+		logger.Warn(msg, fields...)
+	}
 }
 
 // Client is the client class for pentaho.
