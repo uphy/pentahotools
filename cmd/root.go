b67c6e6d2064a0b9a63c9d5a049a1f2c74034045
Yuhi ishikura <yuhi.ishikura.st@hitachi.com>
2017-11-15 16:04:55 +0900

    Add command execution log.

--- cmd/root.go
+++ cmd/root.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 
+	"go.uber.org/zap"
+
 	client "github.com/uphy/pentahotools/client"
 
 	"strings"
@@ -69,8 +71,11 @@ var RootCmd = &cobra.Command{
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := RootCmd.Execute(); err != nil {
+		Client.Logger.Error("Failed to execute command.", zap.Strings("command", os.Args), zap.String("error", err.Error()))
 		fmt.Println(err)
 		os.Exit(1)
+	} else {
+		Client.Logger.Debug("Successfully executed command.", zap.Strings("command", os.Args))
 	}
 }
 
